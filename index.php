8f00a40f2a504d2ddd890270cbd8e7357db56249
BulatSRC <4134236@gmail.com>
2020-06-16 17:33:13 +0500

    module2-task1

--- index.php
+++ index.php
@@ -97,9 +97,9 @@ $announcements = [
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
             <?php foreach ($categories as $item): ?>
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=$item;?></a>
-            </li>
+                <li class="promo__item promo__item--boards">
+                    <a class="promo__link" href="pages/all-lots.html"><?=$item;?></a>
+                </li>
             <?php endforeach; ?>
         </ul>
     </section>
@@ -112,15 +112,25 @@ $announcements = [
         <?php foreach ($announcements as $key => $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$item['picture-url'];?>" width="350" height="260" alt="">
+                    <?php if ($item['picture-url']): ?>
+                        <img src="<?=$item['picture-url'];?>" width="350" height="260" alt="">
+                    <?php endif ?>
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$item['category'];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['title'];?></a></h3>
+                        <?php if ($item['category']): ?>
+                            <span class="lot__category"><?=$item['category'];?></span>
+                        <?php endif ?>
+                    <h3 class="lot__title">
+                        <?php if ($item['title']): ?>
+                            <a class="text-link" href="pages/lot.html"><?=$item['title'];?></a>
+                        <?php endif ?>
+                    </h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$item['price'];?><b class="rub">р</b></span>
+                            <?php if ($item['price']): ?>
+                                <span class="lot__cost"><?=$item['price'];?><b class="rub">р</b></span>
+                            <?php endif ?>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -139,9 +149,9 @@ $announcements = [
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
             <?php foreach ($categories as $item): ?>
-            <li class="nav__item">
-                <a href="pages/all-lots.html"><?=$item;?></a>
-            </li>
+                <li class="nav__item">
+                    <a href="pages/all-lots.html"><?=$item;?></a>
+                </li>
             <?php endforeach; ?>
         </ul>
     </nav>
